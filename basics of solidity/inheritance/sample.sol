344b05f197d35ffffa51170a1f991bc16e981caf
vishwajeeta <gs699542@gmail.com>
2024-03-22 23:52:10 +0000

    simple inheritance

---
+++ basics of solidity/inheritance/sample.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract sample{
+    string public name="vishwa";
+
+    
+}
---
+++ basics of solidity/inheritance/sample1.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {sample} from "./sample.sol";
+contract sample1 is sample{
+
+}
